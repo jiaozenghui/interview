8ec7d806d2661e476164095d90429ccb0b35e2de
jiaozh1 <jiaozh1@lenovo.com>
2024-04-07 14:18:52 +0800

    add interface change

--- backend/app.py
+++ backend/app.py
@@ -41,8 +41,8 @@ def list():
 @app.route("/api/add", methods=["POST", "GET"])
 def add():
 
-    name = request.form.get("name")
-    gender = request.form.get("gender")
+    name = request.form["name"]
+    gender = request.form["gender"]
     print(name)
     if request.method == "POST":
         new_task = User(name=name,gender= gender )
