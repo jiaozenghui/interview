093da33f235fc00476cdaa829d73ca449be501eb
jiaozh1 <jiaozh1@lenovo.com>
2024-04-07 18:38:14 +0800

    add interface change

--- backend/app.py
+++ backend/app.py
@@ -45,9 +45,9 @@ def list():
     size = request.args.get('size', 1, type = int)
     print(size)
     pagination = User.query.paginate(page= page, per_page=size)
-    #user_dict_list = [user.dictRepr() for user in pagination.items]
+    user_dict_list = [user.dictRepr() for user in pagination.items]
     total = pagination.pages
-    return res({"list":pagination.items}, "ok", 0)
+    return res({"list":user_dict_list}, "ok", 0)
 
 
 @app.route("/api/add", methods=["POST", "GET"])
