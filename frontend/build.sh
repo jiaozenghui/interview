c0ca3387613385f5cb71452e0184f428f26b6f96
jiaozh1 <jiaozh1@lenovo.com>
2024-04-08 09:22:45 +0800

    build sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,10 +1,3 @@
-npm install -g increase-memory-limit
-/usr/local/nodejs/bin/increase-memory-limit
-if [ $? -eq 0 ];then
-   echo "increase-momory limit OK"
-else
-   exit
-fi
 # npm install
 npm i
 if [ $? -eq 0 ];then
@@ -14,7 +7,7 @@ else
 fi
 
 #npm audit fix
-ng build --prod
+npm run build --prod
 if [ $? -eq 0 ];then
    echo "ng build OK"
 else
