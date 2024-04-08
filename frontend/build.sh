463d157a605d1a2fcec29e321da3d72ee46c6b29
jiaozh1 <jiaozh1@lenovo.com>
2024-04-08 14:32:19 +0800

    add build folder

--- frontend/build.sh
+++ frontend/build.sh
@@ -26,7 +26,7 @@ fi
 
 rm -f /usr/local/nginx/conf/nginx.conf
 
-sudo cp  -f nginx.conf /usr/local/nginx/conf/nginx.conf
+sudo cp  -f /root/project/interview/frontend/nginx.conf /usr/local/nginx/conf/nginx.conf
 if [ $? -eq 0 ];then
    echo "cp -R nginx.conf OK"
    cd ..
